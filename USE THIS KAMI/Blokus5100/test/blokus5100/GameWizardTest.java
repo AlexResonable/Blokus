08544532a104fb142fb51fe41a5013a2c0748cf7
kamijean <kamijean@kamijean-NV53A.(none)>
2013-03-21 20:22:49 -0600

    Adding JUnit tests

---
+++ USE THIS KAMI/Blokus5100/test/blokus5100/GameWizardTest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package blokus5100;
+
+import java.awt.event.ActionEvent;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author kamijean
+ */
+public class GameWizardTest {
+    
+    public GameWizardTest() {
+    }
+
+    /**
+     * Test of run method, of class GameWizard.
+     */
+    @Test
+    public void testRun() {
+        System.out.println("run");
+        GameWizard instance = new GameWizard();
+        instance.run();
+    }
+
+}
---
+++ USE THIS KAMI/Blokus5100/test/blokus5100/MainMenuTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package blokus5100;
+
+import javax.swing.ImageIcon;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author kamijean
+ */
+public class MainMenuTest {
+    
+    public MainMenuTest() {
+    }
+
+    /**
+     * Test of createAndShowGUI method, of class MainMenu.
+     */
+    @Test
+    public void testCreateAndShowGUI() {
+        System.out.println("createAndShowGUI");
+        MainMenu.createAndShowGUI();
+    }
+
+    /**
+     * Test of createImageIcon method, of class MainMenu.
+     */
+//    @Test
+//    public void testCreateImageIcon() {
+//        System.out.println("createImageIcon");
+//        String path = "";
+//        ImageIcon expResult = null;
+//        ImageIcon result = MainMenu.createImageIcon(path);
+//        assertEquals(expResult, result);
+//    }
+
+    /**
+     * Test of main method, of class MainMenu.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        MainMenu.main(args);
+    }
+}
